583d16473a099f331ea97cefefdc44b3af4903dc
Leonid Bugaenko <bugaenko.leonid@gmail.com>
2025-02-04 16:20:35 +0400

    Исправлена настройка API маршрутов

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,10 @@
 use App\Http\Controllers\Api\HouseController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/houses/search', [HouseController::class, 'search']); 
